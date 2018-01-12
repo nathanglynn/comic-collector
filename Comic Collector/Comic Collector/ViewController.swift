3eb984bfc4f8224581c7cf9cd109856fbe7405cc
Nathan Glynn <nathanglynn@gmail.com>
2018-01-12 13:52:04 -0800

    initial commit

---
+++ Comic Collector/Comic Collector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Comic Collector
+//
+//  Created by Nathan Glynn on 1/12/18.
+//  Copyright © 2018 Nathan Glynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
